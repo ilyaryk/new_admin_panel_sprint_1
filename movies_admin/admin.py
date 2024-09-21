60987b812cb2f6a460efc6edcc913a352e6c4af5
ilya <ilya.rykovanov@gmail.com>
2024-09-21 19:34:03 +0700

    1

--- movies_admin/admin.py
+++ movies_admin/admin.py
@@ -1,25 +1,49 @@
 from django.contrib import admin
-from .models import Genre, Filmwork, GenreFilmwork
+from .models import (Genre, Filmwork, GenreFilmwork,
+                     Person, PersonFilmwork)
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('full_name',
+                    'created', 'modified')
+    search_fields = ('title', 'description', 'id')
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    pass 
+    list_display = ('name', 'description',
+                    'created', 'modified')
+    search_fields = ('name', 'description', 'id')
 
 
 class GenreFilmworkInline(admin.TabularInline):
     model = GenreFilmwork
 
 
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmwork
+
+
 @admin.register(Filmwork)
 class FilmworkAdmin(admin.ModelAdmin):
     inlines = (GenreFilmworkInline,)
-
-    # Отображение полей в списке
-    list_display = ('title', 'type', 'creation_date', 'rating', 'created', 'modified')
-
-    # Фильтрация в списке
+    list_display = ('title', 'type',
+                    'creation_date', 'rating',
+                    'created', 'modified')
     list_filter = ('type',)
+    search_fields = ('title', 'description', 'id')
 
-    # Поиск по полям
-    search_fields = ('title', 'description', 'id') 
+
+@admin.register(PersonFilmwork)
+class PersonFilmworkAdmin(admin.ModelAdmin):
+    list_display = ('film_work', 'genre', 'created')
+    list_filter = ('film_work', 'genre',)
+    search_fields = ('film_work', 'genre', 'id')
+
+
+@admin.register(GenreFilmwork)
+class GenreFilmworkAdmin(admin.ModelAdmin):
+    list_display = ('film_work', 'genre', 'created')
+    list_filter = ('film_work', 'genre',)
+    search_fields = ('film_work', 'genre', 'id')
