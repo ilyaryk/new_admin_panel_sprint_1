af1dba115e61b91fb038bdf469b43f7563a1e0eb
ilya <ilya.rykovanov@gmail.com>
2024-09-21 19:17:42 +0700

    1

--- movies_admin/models.py
+++ movies_admin/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.translation import gettext_lazy as _
 
+
 class TimeStampedMixin(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
@@ -16,24 +17,18 @@ class UUIDMixin(models.Model):
 
     class Meta:
         abstract = True
- 
+
 
 class Genre(UUIDMixin, TimeStampedMixin):
-    # Типичная модель в Django использует число в качестве id. В таких ситуациях поле не описывается в модели.
-    # Вам же придётся явно объявить primary key.
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # Первым аргументом обычно идёт человекочитаемое название поля
     name = models.CharField('name', max_length=255)
-    # blank=True делает поле необязательным для заполнения.
     description = models.TextField('description', blank=True)
 
     def __str__(self):
-        return self.name 
+        return self.name
 
     class Meta:
-        # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
         db_table = "content\".\"genre"
-        # Следующие два поля отвечают за название модели в интерфейсе
         verbose_name = 'Жанр'
         verbose_name_plural = 'Жанры'
 
@@ -43,22 +38,23 @@ class Filmwork(UUIDMixin, TimeStampedMixin):
         MOVIE = "movie"
         TV_SHOW = "tv_show"
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    file_path = models.FileField(_('file'), blank=True, null=True, upload_to='admin_movies_files/')
+    file_path = models.FileField(_('file'),
+                                 blank=True, null=True,
+                                 upload_to='admin_movies_files/')
     title = models.CharField(_('title'), max_length=255, null=False)
     genres = models.ManyToManyField(Genre, through='GenreFilmwork')
     description = models.TextField('description', blank=True)
     creation_date = models.DateTimeField('creation_date')
     rating = models.FloatField('rating', blank=True,
                                validators=[MinValueValidator(0),
-                                           MaxValueValidator(100)]) 
+                                           MaxValueValidator(100)])
     type = models.CharField('type', choices=TypesChoices.choices)
+
     def __str__(self):
-        return self.title 
+        return self.title
 
     class Meta:
-        # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
         db_table = "content\".\"film_work"
-        # Следующие два поля отвечают за название модели в интерфейсе
         verbose_name = 'Фильм'
         verbose_name_plural = 'Фильмы'
 
